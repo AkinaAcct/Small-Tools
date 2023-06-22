b53774ee89546a019831af015461263d0f024c93
nya-sec <nyaisacat@gmail.com>
2023-06-23 00:05:42 +0800

    i

---
+++ GitInfo/gitinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#by nya
+
+LINES=$()$(git ls-files | xargs wc -l | tail -n1 | awk '{print $1}') #lines of code
+STATUS=$(git status -s) #git status -s
+if [ -z "${STATUS}" ];then
+    STATUS="无文件变更"
+fi
+OBJSIZE=$(git count-objects -H) #objects' full size
+ALLLOCALBRANCH=$(git branch) #git branch
+ALLREMOTEBRANCH=$(git branch -r) #git branch -r
+
+ALLINFO=('${LINES}' '${STATUS}' '${ALLLOCALBRANCH}' '${ALLREMOTEBRANCH}' '${OBJSIZE}') #all info
