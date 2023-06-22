79c1496fe6d59c4fcef8b427a73bcf7c261220ef
nya-sec <nyaisacat@gmail.com>
2023-06-23 00:50:18 +0800

    i

--- GitInfo/gitinfo.sh
+++ GitInfo/gitinfo.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #by nya
 
-LINES=$()$(git ls-files | xargs wc -l | tail -n1 | awk '{print $1}') #lines of code
+ARGNUM=6
+
+LINES=$(git log  --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "%s", add, subs, loc }' -) #lines of code
 STATUS=$(git status -s) #git status -s
 if [ -z "${STATUS}" ];then
     STATUS="无文件变更"
 fi
 OBJSIZE=$(git count-objects -H) #objects' full size
 ALLLOCALBRANCH=$(git branch) #git branch
-ALLREMOTEBRANCH=$(git branch -r) #git branch -r
+CONTRIBUTORS=$(git log --pretty='%aN' | sort -u) #contributors
+LOCALCOMMITS=$(git log --oneline | wc -l) #local commit timez
 
-ALLINFO=('${LINES}' '${STATUS}' '${ALLLOCALBRANCH}' '${ALLREMOTEBRANCH}' '${OBJSIZE}') #all info
+ALLINFO=("${LINES}" "${STATUS}" "${ALLLOCALBRANCH}" "${OBJSIZE}" "${CONTRIBUTORS}" "${LOCALCOMMITS}") #all info var
+ALLINFOTEXT=("lines_of_code" "status" "local_branch" "full_size" "contributors" "local_commit_times") #all info text
 
-for i in "${ALLINFO[@]}";do
-    echo "$i"
+for i in $(seq 0 $(($ARGNUM - 1)));do
+    echo -e "\e[32m${ALLINFOTEXT[$i]}: \e[0m"
+    echo -e "\t"${ALLINFO[$i]}""
 done
