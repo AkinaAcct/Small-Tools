094025072399ed87edc54ac5a7868d1e13a55a49
nya-sec <nyaisacat@gmail.com>
2023-07-12 09:22:00 +0800

    add communit

--- Termux-scripts/time.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#by nya
-TERMUX_DIALOG_TIME=$(termux-dialog time | grep text | awk '{print $2}' | sed 's/\"//g')
-TERMUX_DIALOG_DATE=$(termux-dialog date | grep text | awk '{print $2,$3,$4}' | sed 's/\"//g')
-CURRENT_TIME_COMMAND=$(date "+%H:%M")
-CURRENT_DATE_COMMAND=$(date "+%a %b %d")
