895df317dded010dfc6a75506817052252048473
nya-sec <nyaisacat@gmail.com>
2023-07-17 17:54:16 +0800

    add adb check

--- Termux-scripts/get_time.sh
+++ Termux-scripts/get_time.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #by nya
+
+source "./adb_connection_check.sh"
+
 TERMUX_DIALOG_TIME=$(termux-dialog time | grep text | awk '{print $2}' | sed 's/\"//g')  #仅作参考
 TERMUX_DIALOG_DATE=$(termux-dialog date | grep text | awk '{print $2,$3,$4}' | sed 's/\"//g')  #仅作参考
 CURRENT_TIME_COMMAND=$(date "+%H:%M")
