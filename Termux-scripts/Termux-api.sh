5473f4904fabf097b81ba921962d83c921d7c071
nya-sec <nyaisacat@gmail.com>
2023-07-17 17:18:19 +0800

    A

--- Termux-scripts/Termux-api.sh
+++ Termux-scripts/Termux-api.sh
@@ -3,8 +3,35 @@
 source "./color_echo.sh" #echo颜色变量存储文件
 source "./adb_connection_check.sh" #修改以下路径来加载自定义路径下的adb连接脚本(可自定义)
 
-adb shell am startservice -n com.termux.api/.KeepAliveService
-EXITSTATUS=$?
-if [[ EXITSTATUS != 0 ]]; then
-    echo -e "${RED}E${RESET}:启动失败.检查你是否安装了Termux-api."
-fi
+start_service(){
+    adb shell am startservice -n com.termux.api/.KeepAliveService
+    EXITSTATUS=$?
+    if [[ ${EXITSTATUS} = 0 ]]; then
+        echo -e "${GREEN}I${RESET}:启动成功."
+    else
+        echo -e "${RED}E${RESET}:启动失败.检查你是否安装了Termux-api."
+    fi
+}
+stop_service(){
+    adb shell am stopservice -n com.termux.api/.KeepAliveService
+    EXITSTATUS=$?
+    if [[ ${EXITSTATUS} = 0 ]]; then
+        echo -e "${GREEN}I${RESET}:停止成功."
+    else
+        echo -e "${RED}E${RESET}:停止失败,或你并未启动Termux-api."
+    fi
+}
+print_help(){
+    echo "-s, 启动Termux-api服务."
+    echo "-p, 停止Termux-api服务."
+    echo "-h, 打印此帮助."
+}
+while getopts :sph OPT; do
+    case ${OPT} in
+    s) start_service;;
+    p) stop_service;;
+    h) print_help;;
+    ?) echo -e "${RED}E${RESET}:未知的选项:-${OPT}.使用-h选项来获取帮助." && exit 1;;
+    *) start_service;;
+esac
+done
--- Termux-scripts/adb_connection_check.sh
+++ Termux-scripts/adb_connection_check.sh
@@ -6,11 +6,8 @@ EXITSTATUS=$?
 if [[ ${EXITSTATUS} != 0 ]]; then
     echo -e "\E[1;31m你没有连接至adb...\E[0m"
     exit 1
+else
+    echo -e "\E[1;32m检测通过.ADB已连接.\E[0m"
 fi
 
 
-for ((i=2;i<=4;i++));do
-    echo -e "\E[1;34m$(seq -s '.' ${i} | sed 's/[0-9]//g')\E[0m"
-    sleep 1
-done
-echo -e "\E[1;32m检测通过.ADB已连接.\E[0m"
