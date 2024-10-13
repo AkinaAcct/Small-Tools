e99fd6dd8069b72048f63596464823796822e7a1
AkinaAcct <108107785+AkinaAcct@users.noreply.github.com>
2024-10-13 23:39:31 +0800

    fix

--- TermuxAutoConfig/TAC.sh
+++ TermuxAutoConfig/TAC.sh
@@ -16,7 +16,7 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsM
 ## 解压
 unzip JBM.zip
 ## 替换字体
-mv JetBrainsMonoNerdFont-Bold.ttf ${HOME}/.termux/font.ttf
+mv JetBrainsMonoNerdFont-Regular.ttf ${HOME}/.termux/font.ttf
 
 # 复位
 termux-wake-unlock
